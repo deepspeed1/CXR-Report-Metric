58110eee9b0b186ac741c233a5e27d003ae8e317
deepspeed1 <ahmed.abdelrashid.2023@gmail.com>
2024-08-13 12:19:19 +0300

    Add files via upload

---
+++ cxr_main.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+from CXRMetric.run_eval import calc_metric
+from huggingface_hub import hf_hub_download, login, HfApi, snapshot_download
+
+
+cwd = os.getcwd()
+
+DOWNLOAD_REPO = 'ahmedabdelwahed/RologyVLM-22k'
+
+login('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')
+
+# gt_reports
+hf_hub_download(
+    filename = 'gt_reports.csv',
+    local_dir = cwd,
+    repo_type = 'model',
+    repo_id = DOWNLOAD_REPO)
+
+# predicted_reports
+hf_hub_download(
+    filename = 'predicted_reports.csv',
+    local_dir = cwd,
+    repo_type = 'model',
+    repo_id = DOWNLOAD_REPO)
+
+os.chdir(f'{cwd}/CXR-Report-Metric')
+# del calc_metric
+
+
+gt_reports = f'{cwd}/gt_reports.csv'
+predicted_reports = f'{cwd}/predicted_reports.csv'
+out_file = f'{cwd}/report_scores.csv'
+use_idf = False
+calc_metric(gt_reports, predicted_reports, out_file, use_idf)
